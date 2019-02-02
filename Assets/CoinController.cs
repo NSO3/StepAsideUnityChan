69ca3f7ee74042290a48f5795f34736290f7214e
NSO3 <zzmdmdns@gmail.com>
2019-02-02 21:48:14 +0900

    Lesson6 コミット

---
+++ Assets/CoinController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinController : MonoBehaviour
+{
+    private GameObject unityChan;
+
+    // Use this for initialization
+    void Start()
+    {
+        //回転を開始する角度を設定
+        this.transform.Rotate(0, Random.Range(0, 360), 0);
+        this.unityChan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //回転
+        this.transform.Rotate(0, 3, 0);
+        if (this.unityChan.transform.position.z -10 > this.gameObject.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/destroyPrefab.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyPrefab : MonoBehaviour {
+
+    private GameObject unityChan;
+
+    // Use this for initialization
+    void Start () {
+        this.unityChan = GameObject.Find("unitychan");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (this.unityChan.transform.position.z - 10 > this.gameObject.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
